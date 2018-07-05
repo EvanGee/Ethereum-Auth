3b999e7402ed7068aa1e6a6b3593f7195f38d071
EvanGee <evangiese77@gmail.com>
2018-07-05 12:13:02 -0600

    Add request Auth method

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 
-var jwt = require('jsonwebtoken');
-var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-
+const axios = require("axios")
 module.exports = class Auth {
 
-    constructor(web3) {
+    constructor(web3, metamask) {
         this.web3 = web3
+        this.metamask = metamask
     }
 
-    sign(data, pass) {
+    sign(data) {
         return new Promise((resolve, reject) => {
             if (this.web3.eth.defaultAccount == null) {
                 reject("default account is not set")
             }
 
-            this.web3.personal.sign(web3.toHex(data), this.web3.eth.defaultAccount, (err, data) => {
-                resolve(data)
-                reject(err)
-            })
-
+            if (this.metamask === true) {
+                this.web3.personal.sign(web3.toHex(JSON.stringify(data)), this.web3.eth.defaultAccount, (err, data) => {
+                    if (err)
+                        reject(err)
+                    resolve(data)
+                })
+            }
 
         })
     }
 
-    recover(msg, address) {
+    validate(token, msg, address) {
         const recovered = web3.eth.accounts.reverse(msg)
         if (recovered[0] !== address) {
             return false
         }
         return true
+    }
+
+
+    requestPermission(url) {
+        const token = "Requesting Access to " + url
+        sign(token).then((signature)=>{
+            axios.post(url, signature)
+        })
     }
 }
